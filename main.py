9d33bca3e8a9503534989c67faf1682c51be1c8d
username <you@example.com>
2024-02-17 19:00:55 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 def main():
-    book_path = "books/frankenstien.txt"
+    book_path = "/home/dan/workspace/github.com/username/Bookbot/books/frankenstien.txt"
     text = get_book_text(book_path)
-    words = count_words(text)
-    letters = count_letters(text)
-    print(letters)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-def count_words(text):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
     words = text.split()
     return len(words)
 
 
+def sort_on(d):
+    return d["num"]
 
-def count_letters(text):
-    lower_text = text.lower()
-    letters_in_text = {}
-    for letter in lower_text:
-        if letter in letters_in_text:
-            letters_in_text[letter] += 1
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            letters_in_text[letter] = 1
-    return letters_in_text
+            chars[lowered] = 1
+    return chars
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 
+main()
 
 main()
